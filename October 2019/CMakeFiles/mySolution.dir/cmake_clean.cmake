bce6b0cc30c1ff29832a97b8c20944d259120326
Tzadiko <tzadsauce@gmail.com>
2019-11-27 22:17:10 -0500

    cmake

---
+++ October 2019/CMakeFiles/mySolution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mySolution.dir/TriTriAgainAgain.cpp.o"
+  "mySolution.pdb"
+  "mySolution.exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mySolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
